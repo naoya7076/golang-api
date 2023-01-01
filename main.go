a41a05fef89deddf22ae58b52b46707d2b66b1b2
naoya7076 <naoya7076@gmail.com>
2023-01-01 22:19:00 +0900

    muxを導入

--- main.go
+++ main.go
@@ -4,18 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/naoya7076/golang-api/handlers"
 )
 
 func main() {
+	r := mux.NewRouter()
 
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ListArticleHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.NiceArticleHandler)
-	http.HandleFunc("/comment", handlers.CommentHandler)
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.ListArticleHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/article/nice", handlers.NiceArticleHandler)
+	r.HandleFunc("/comment", handlers.CommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
