ad60a14e2f01c7aa8e91dadc6e98c2833a0c6e82
naoya7076 <naoya7076@gmail.com>
2023-01-01 22:45:59 +0900

    パスパラメータの取得

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
 	r.HandleFunc("/article/list", handlers.ListArticleHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/1", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article/nice", handlers.NiceArticleHandler).Methods(http.MethodPost)
 	r.HandleFunc("/comment", handlers.CommentHandler).Methods(http.MethodPost)
 
