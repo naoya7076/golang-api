c1919cb80cd6786e8a2b1033d45fa21f73d946ca
naoya7076 <naoya7076@gmail.com>
2023-01-29 15:47:41 +0900

    article_serviceの実装

--- services/article_service.go
+++ services/article_service.go
@@ -1,8 +1,6 @@
 package services
 
 import (
-	"database/sql"
-
 	"github.com/naoya7076/golang-api/models"
 	"github.com/naoya7076/golang-api/repositories"
 )
@@ -13,15 +11,50 @@ func GetArticleService(articleID int) (models.Article, error) {
 		return models.Article{}, err
 	}
 	defer db.Close()
-	article, err := repositories.SelectArticleDetail(&sql.DB{}, articleID)
+	article, err := repositories.SelectArticleDetail(db, articleID)
 	if err != nil {
 		return models.Article{}, err
 	}
-	commentList, err := repositories.SelectCommentList(sql.DB{}, articleID)
+	commentList, err := repositories.SelectCommentList(db, articleID)
 	if err != nil {
 		return models.Article{}, err
 	}
 
 	article.CommentList = append(article.CommentList, commentList...)
+	return article, nil
+}
+
+func PostArticleSertice(article models.Article) (models.Article, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Article{}, err
+	}
+	newArticle, err := repositories.InsertArticle(db, article)
+	if err != nil {
+		return models.Article{}, err
+	}
+	return newArticle, nil
+}
+
+func GetArticleListService(page int) ([]models.Article, error) {
+	db, err := connectDB()
+	if err != nil {
+		return []models.Article{}, err
+	}
+	articleList, err := repositories.SelectArticleList(db, page)
+	if err != nil {
+		return []models.Article{}, err
+	}
+	return articleList, nil
+}
+
+func PostNiceService(article models.Article) (models.Article, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Article{}, err
+	}
+	if err := repositories.UpdateNiceNum(db, article.ID); err != nil {
+		return models.Article{}, err
+	}
 	return article, nil
 }
