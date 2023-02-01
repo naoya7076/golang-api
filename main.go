6450a8baba8c4dbf3da798644dde03a4aa38ad68
naoya7076 <naoya7076@gmail.com>
2023-02-02 08:40:52 +0900

    DB接続の処理をmainに移す

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/mux"
-	"github.com/naoya7076/golang-api/handlers"
+	"github.com/naoya7076/golang-api/controllers"
+	"github.com/naoya7076/golang-api/services"
+)
+
+var (
+	dbUser     = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbDatabase = os.Getenv("DB_NAME")
+	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 )
 
 func main() {
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ser := services.NewMyAppService(db)
 	con := controllers.NewMyAppController(ser)
 
 	r := mux.NewRouter()
