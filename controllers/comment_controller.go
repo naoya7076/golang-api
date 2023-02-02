e62d0551165c292874a8cf471a0e357a4ceb8805
naoya7076 <naoya7076@gmail.com>
2023-02-02 21:44:49 +0900

    CommentControllerの実装

---
+++ controllers/comment_controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/naoya7076/golang-api/controllers/services"
+	"github.com/naoya7076/golang-api/models"
+)
+
+type CommentController struct {
+	service services.CommentServicer
+}
+
+func NewCommentController(s services.CommentServicer) *CommentController {
+	return &CommentController{service: s}
+}
+
+func (c *MyAppController) CommentHandler(w http.ResponseWriter, req *http.Request) {
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+	comment, err := c.service.PostCommentService(reqComment)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(comment)
+}
