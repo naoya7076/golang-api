76f0aa923d4b46822ca060da336a80b19cb6a6a0
naoya7076 <naoya7076@gmail.com>
2023-02-02 22:44:29 +0900

    fix arg db

--- repositories/comment_test.go
+++ repositories/comment_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestSelectCommentList(t *testing.T) {
 	expectedNum := 2
-	got, err := repositories.SelectCommentList(*testDB, 1)
+	got, err := repositories.SelectCommentList(testDB, 1)
 	if err != nil {
 		t.Fatal(err)
 	}
