4126e941d6a3f878a9a1a63d958b2a03cdaf6a52
naoya7076 <naoya7076@gmail.com>
2023-02-02 21:43:34 +0900

    ArticleControllerの実装

---
+++ controllers/article_controller.go
@@ -0,0 +1,85 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/naoya7076/golang-api/controllers/services"
+	"github.com/naoya7076/golang-api/models"
+)
+
+type ArticleController struct {
+	service services.ArticleServicer
+}
+
+func NewArticleController(s services.ArticleServicer) *ArticleController {
+	return &ArticleController{service: s}
+}
+
+func (c *ArticleController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+
+	article, err := c.service.PostArticleService(reqArticle)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(article)
+}
+
+func (c *ArticleController) ListArticleHandler(w http.ResponseWriter, req *http.Request) {
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	articleList, err := c.service.GetArticleListService(page)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(articleList)
+}
+
+func (c *ArticleController) ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleId, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
+
+	article, err := c.service.GetArticleService(articleId)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(article)
+}
+
+func (c *ArticleController) NiceArticleHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+	article, err := c.service.PostNiceService(reqArticle)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(article)
+}
