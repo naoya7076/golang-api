3ba9c392191eb547258805436d092970e087fde1
naoya7076 <naoya7076@gmail.com>
2023-02-02 21:50:17 +0900

    routeの修正

--- routers/router.go
+++ routers/router.go
@@ -7,14 +7,14 @@ import (
 	"github.com/naoya7076/golang-api/controllers"
 )
 
-func NewRouter(con *controllers.MyAppController) *mux.Router {
+func NewRouter(aCon *controllers.ArticleController, cCon *controllers.CommentController) *mux.Router {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", con.ListArticleHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", con.NiceArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", con.CommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article", aCon.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", aCon.ListArticleHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", aCon.NiceArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", cCon.CommentHandler).Methods(http.MethodPost)
 
 	return r
 }
