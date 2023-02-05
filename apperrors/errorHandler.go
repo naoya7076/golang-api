8ef2c4a72e7f8b07cc7af3c5a6949f4e2d95f130
naoya7076 <naoya7076@gmail.com>
2023-02-05 16:52:22 +0900

    errorHandlerの実装

--- apperrors/errorHandler.go
+++ apperrors/errorHandler.go
@@ -1,7 +1,32 @@
 package apperrors
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
 
 func ErrorHandler(w http.ResponseWriter, req *http.Request, err error) {
+	var appErr *MyAppError
+	if !errors.As(err, &appErr) {
+		appErr = &MyAppError{
+			ErrCode: Unknown,
+			Message: "internal process failed",
+			Err:     err,
+		}
+	}
 
+	var statusCode int
+
+	switch appErr.ErrCode {
+	case NAData:
+		statusCode = http.StatusNotFound
+	case NoTargetData, ReqBodyDecodeFailed, BadParams:
+		statusCode = http.StatusBadRequest
+	default:
+		statusCode = http.StatusInternalServerError
+	}
+
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(appErr)
 }
