ff397522bb64fbdd6fe1d58281affbed05b5d162
naoya7076 <naoya7076@gmail.com>
2023-02-07 21:20:09 +0900

    GetArticleServiceを並行処理

--- services/article_service.go
+++ services/article_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"database/sql"
 	"errors"
+	"sync"
 
 	"github.com/naoya7076/golang-api/apperrors"
 	"github.com/naoya7076/golang-api/models"
@@ -10,19 +11,43 @@ import (
 )
 
 func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) {
-	article, err := repositories.SelectArticleDetail(s.db, articleID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			err = apperrors.NAData.Wrap(err, "no data")
-			return models.Article{}, err
+	var article models.Article
+	var commentList []models.Comment
+	var articleGerErr, commentGetErr error
+
+	var amu sync.Mutex
+	var cmu sync.Mutex
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func(db *sql.DB, articleID int) {
+		defer wg.Done()
+		amu.Lock()
+		article, articleGerErr = repositories.SelectArticleDetail(db, articleID)
+		amu.Unlock()
+	}(s.db, articleID)
+
+	go func(db *sql.DB, articleID int) {
+		defer wg.Done()
+		cmu.Lock()
+		commentList, commentGetErr = repositories.SelectCommentList(db, articleID)
+		cmu.Unlock()
+	}(s.db, articleID)
+
+	wg.Wait()
+	if articleGerErr != nil {
+		if errors.Is(articleGerErr, sql.ErrNoRows) {
+			articleGerErr = apperrors.NAData.Wrap(articleGerErr, "no data")
+			return models.Article{}, articleGerErr
 		}
-		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
-		return models.Article{}, err
+		articleGerErr = apperrors.GetDataFailed.Wrap(articleGerErr, "fail to get data")
+		return models.Article{}, articleGerErr
 	}
-	commentList, err := repositories.SelectCommentList(s.db, articleID)
-	if err != nil {
-		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
-		return models.Article{}, err
+
+	if commentGetErr != nil {
+		commentGetErr = apperrors.GetDataFailed.Wrap(commentGetErr, "fail to get data")
+		return models.Article{}, commentGetErr
 	}
 
 	article.CommentList = append(article.CommentList, commentList...)
